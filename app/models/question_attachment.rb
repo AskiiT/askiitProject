d8f91ff5ab99511ebbf2d580d501a563047befc6
Luis Ernesto Gil <luegilca@unal.edu.co>
2017-03-23 23:35:50 -0500

    Added QuestionAttachment validations through paperclip gem

--- app/models/question_attachment.rb
+++ app/models/question_attachment.rb
@@ -1,3 +1,7 @@
 class QuestionAttachment < ApplicationRecord
   belongs_to :question
+
+  has_attached_file :attachment, styles: { medium: "1280x720", thumb: "800x600", mini: "400x200" }
+  #Validates ONLY (so far) image's data types.
+  validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\Z/
 end
