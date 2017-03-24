f8ff7967d043f9ec56fc2164c947c4ed9be3848c
jsviveroj <jsviveroj@unal.edu.co>
2017-03-24 12:00:57 -0500

    Validation for Question.date_posted added and tested

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,6 +11,12 @@ class Question < ApplicationRecord
   validates :title, length: { in: 12..140 }
   validates_inclusion_of :difficulty, in: 1..10
   validates :body, length: {maximum: 500}, allow_blank: true
+  validate  :actual_date
 
+  def actual_date
+      if date_posted.present? && date_posted > Date.today
+        errors.add(:date_posted, "can't be post in the future" )
+      end
+  end
 
 end
