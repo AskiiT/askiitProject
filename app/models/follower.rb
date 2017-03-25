888beabe77f7f9ca949d1d083e76ae46d77392ee
jsviveroj <jsviveroj@unal.edu.co>
2017-03-25 12:12:17 -0500

    Fixed validation for Following itself

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,4 +1,16 @@
 class Follower < ApplicationRecord
-   belongs_to :followed, :class_name => :User
+  belongs_to :followed, :class_name => :User
   belongs_to :follower, :class_name => :User
+
+  validate :following_me
+
+  def following_me
+	   if followed_id && follower_id
+		     if follower_id == followed_id
+			        errors.add(:followed_id, "You can't follow yourself")
+		     end
+	   end
+  end
+
+
 end
