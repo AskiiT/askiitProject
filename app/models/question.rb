acd874117f02c0a8635068bc6dc0561de29b1811
jsviveroj <jsviveroj@unal.edu.co>
2017-03-25 11:32:42 -0500

    Fixed on_destroy relations with cascade

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :topic
-  has_many :question_attachments,               :dependent => :destroy
+  has_many :question_attachments,
   has_many :tags, through: :question_has_tags
   has_many :question_has_tags
   has_many :users, through: :postulates
--- db/migrate/20170324041232_create_question_attachments.rb
+++ db/migrate/20170324041232_create_question_attachments.rb
@@ -1,7 +1,7 @@
 class CreateQuestionAttachments < ActiveRecord::Migration[5.0]
   def change
     create_table :question_attachments do |t|
-      t.references :question, foreign_key: true
+      t.references :question, foreign_key: true, on_destroy: :cascade
 
       t.timestamps
     end
